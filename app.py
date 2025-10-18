bff1d6f93df1bb661badf6bbc21f309f0ed88586
khmerweb <vuthdevelop1@gmail.com>
2025-10-18 17:42:27 +0700

    Make it better

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 # save this as app.py
 from flask import Flask
+from index import index
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+app.register_blueprint(index, url_prefix='/')
