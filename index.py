393399b7b97cff43c79d3ddaddcd67878b3fb78e
khmerweb <vuthdevelop1@gmail.com>
2025-10-19 14:04:01 +0700

    Make it better

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 
-Index = Blueprint('Index', __name__)
+frontend = Blueprint('frontend', __name__)
 
-@Index.route('/')
+@frontend.route('/')
 def home():
     return render_template("index.html")
